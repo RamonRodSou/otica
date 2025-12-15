3ffd6b7d3d5431ea7b3b80564aaccdd09444d01b
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-12-15 19:37:13 -0300

    feat: colocando navegacao no voucher

--- src/component/vantagem/index.tsx
+++ src/component/vantagem/index.tsx
@@ -4,6 +4,13 @@ import voucherImg from '../../asserts/img/voucher.png'
 
 export default function Vantagem() {
 
+    async function handleSubmit(): Promise<void> {
+        const element = document.getElementById('faleConosco');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' }); 
+        }
+    }
+
     return (
         <Box className="vantagem">
             <Box className="infor">
@@ -13,7 +20,7 @@ export default function Vantagem() {
                 <Typography variant='body1' className='subtitle'>
                     Não perca a chance de acessar benefícios exclusivos: Preencha nosso formulário, agende sua visita e receba seu voucher em minutos. Tudo rápido e fácil!
                 </Typography>
-                <Box className="image">
+                <Box className="image" onClick={handleSubmit}>
                     <img src={voucherImg} alt="Vohcer 33 porcentos" width='250' height='100'/>
                 </Box>
             </Box>
