2f0e96a47267f457d54dab9df930c531120f624f
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-12-15 23:27:24 -0300

    feat: mudando o h1

--- src/component/hero/index.tsx
+++ src/component/hero/index.tsx
@@ -8,21 +8,19 @@ export default function Hero() {
     return (
         <Box className="hero">
             <Box className="apresentacao">
-                <Typography variant='h2' className="title">ARMAÇÃO</Typography>
-                <Typography variant='h2' className="title">+</Typography>
-
+                <Typography variant='h2' className="title">EXAME</Typography>
                 <Typography variant='h2' className="title"> GRÁTIS</Typography>
             </Box>
             <Box className="valor">
                 <Box className='apenas'>
-                    <span>Exame por</span>
-                    <span>apenas</span>
-                    {/* <span>10x de</span> */}
+                    <span>Armações</span>
+                    <span>a partir</span>
+                    <span>de</span>
                 </Box>
                 
                 <Box className='promocao'>
-                    <Typography variant='h2'>29</Typography>
-                    <Typography variant='body1'>,90</Typography>
+                    <Typography variant='h2'>99</Typography>
+                    <Typography variant='body1'>,99</Typography>
                 </Box>
             </Box>
             <Box>
